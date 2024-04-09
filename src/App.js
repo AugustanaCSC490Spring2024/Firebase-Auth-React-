67a98e05f300dd5f1adb1cc9b505c1fd478711ba
LoganHansen20 <113001105+Dirtbike140@users.noreply.github.com>
2024-04-09 09:37:00 -0500

    Added Auth code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import AuthDetails from './components/AuthDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn/>
+      <SignUp/>
+      <AuthDetails/>
+    </div>
+  );
+}
+
+export default App;
